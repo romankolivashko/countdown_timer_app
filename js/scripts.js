633a8d29ad49ed71aad223e0367bd6f729f67fb0
Roman Kolivashko <rkolivashko@gmail.com>
2021-08-15 12:11:23 -0700

    WIP: business logic is tested

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,16 @@ function countdown() {
   const newYearsDate = new Date(newYears);
   const currentDate = new Date();
 
-  const seconds = (newYearsDate - currentDate) * 1000;
+  const totalSeconds = (newYearsDate - currentDate) / 1000;
 
-  console.log(newYearsDate - currentDate);
-  const days = Math.floor(seconds / 3600 / 24);
-
-  console.log(days);
+  // console.log(newYearsDate - currentDate);
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const minutes = Math.floor(totalSeconds / 60) % 24;
+  const seconds = Math.floor(totalSeconds) % 60;
+  
+  
+  console.log(days, hours, minutes, seconds);
 }
 
 //initial call
