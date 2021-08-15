0ff97c93acd05a25a0759d61ff402a51ea6b7969
Roman Kolivashko <rkolivashko@gmail.com>
2021-08-15 12:41:21 -0700

    BUGFIX: fixed null returned value

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
+window.onload = function()
+{
+
+const daysElem = document.getElementById('days');
+const hoursElem = document.getElementById('hours');
+const minutesElem = document.getElementById('minutes');
+const secondsElem = document.getElementById('seconds');
+
 const newYears = '1 Jan 2022';
 
 function countdown() {
@@ -6,16 +14,23 @@ function countdown() {
 
   const totalSeconds = (newYearsDate - currentDate) / 1000;
 
-  // console.log(newYearsDate - currentDate);
+  
   const days = Math.floor(totalSeconds / 3600 / 24);
   const hours = Math.floor(totalSeconds / 3600) % 24;
   const minutes = Math.floor(totalSeconds / 60) % 24;
   const seconds = Math.floor(totalSeconds) % 60;
-  
-  
+
+  daysElem.innerHTML = days;
+  hoursElem.innerHTML = hours;
+  minutesElem.innerHTML = minutes;
+  secondsElem.innerHTML - seconds;
+
+  // console.log(newYearsDate - currentDate);
   console.log(days, hours, minutes, seconds);
 }
 
 //initial call
 countdown();
 setInterval(countdown, 1000);
+
+}
